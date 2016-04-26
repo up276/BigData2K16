3c3951ea64b88b8deeb967d315db0c8dee400ce2
Christina <christina.bogdan@gmail.com>
2016-04-26 16:55:56 -0400

    new citi

--- citi_map.py
+++ citi_map.py
@@ -8,6 +8,36 @@ final_cols = ['tripduration', 'starttime', 'stoptime', 'start station id', \
         'start station longitude', 'end station id', 'end station name', 'end station latitude', \
         'end station longitude', 'bikeid', 'usertype', 'birth year', 'gender']
 new_cols.extend(final_cols)
+rd = {'bikeid': 11,
+ 'birth year': 13,
+ 'end station id': 7,
+ 'end station latitude': 9,
+ 'end station longitude': 10,
+ 'end station name': 8,
+ 'gender': 14,
+ 'start station id': 3,
+ 'start station latitude': 5,
+ 'start station longitude': 6,
+ 'start station name': 4,
+ 'starttime': 1,
+ 'stoptime': 2,
+ 'tripduration': 0,
+ 'usertype': 12}
+
+pivot_cols = {'bikeid':0,
+        'birth year':1,
+        'station id':3,
+        'latitude':4,
+        'longitude':5,
+        'station name':6,
+        'launch time':7,
+        'tripduration':8,
+        'usertype':2,
+        'gender':9}
+cf =['YR--MODAHR','year','month','day','hour','bikeid','birth_year','usertype',\
+        'station_id','latitude','longitude','station_name','launch_time'\
+        ,'tripduration','usertype','gender']
+          'launch_time','station_id','latitude','longitude','bikeid','usertype','birth_year','gender']
 
 def split_date(st):
     split =  filter(None, re.split("[ :/]+",st))[:4]
@@ -24,15 +54,36 @@ def zero_pad(x):
     else:
         return str(x)
 
-print(','.join(new_cols))
+def get_l_start(l):
+    l_start =[l[rd['bikeid']],l[rd['birth year']],l[rd['usertype']],\
+            l[rd['start station id']],l[rd['start station latitude']],\
+            l[rd['start station longitude']],l[rd['start station name']],\
+            l[rd['starttime']],l[rd['tripduration']],\
+            l[rd['gender']]]
+    return l_start
+
+def get_l_end(l):
+    l_end =[l[rd['bikeid']],l[rd['birth year']],l[rd['usertype']],\
+            l[rd['end station id']],l[rd['end station latitude']],\
+            l[rd['end station longitude']],l[rd['end station name']],\
+            l[rd['stoptime']],l[rd['tripduration']],\
+            l[rd['gender']]]
+    return l_end
+
+def add_print(l):
+    date_l = split_date(l[pivot_cols['launch time']])
+    l_key = "".join(zero_pad(i) for i in date_l)
+    date_l.extend(l)
+    date_l.insert(0,l_key)
+    sys.stdout.write(','.join(date_l))
+
+
+print(','.join(cf))
 for l in sys.stdin:
     l = l.split(',')
+    l_start = get_l_start(l)
+    l_end = get_l_end(l)
     if any("tripduration" in w for w in l):
         continue
-    start = split_date(l[1])
-    start_key = "".join(zero_pad(i) for i in start)
-    end = split_date(l[2])
-    end.extend(l)
-    start.extend(end)
-    start.insert(0,start_key)
-    sys.stdout.write(','.join(start))
+    add_print(l_start)
+    add_print(l_end)
