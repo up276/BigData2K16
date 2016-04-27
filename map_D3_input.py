b9928ab11a48f66d69f94a158a963771bc2bd266
Vincent Chabot <vec241@login-1-1.local>
2016-04-27 17:24:22 -0400

    uploading the right columns in map_D3_input.py

--- map_D3_input.py
+++ map_D3_input.py
@@ -5,13 +5,13 @@ for row in sys.stdin:
     splits=row.split(",")
 
  	# Change according to correct data location
-    station_id = splits[2]
-	action = splits[1] # pickup or dropoff
-	rain = splits[2] # rain or not
-	temperature = splits[3]
-	hour = splits[4]
-	latitude = = splits[5]
-	longitude = = splits[5]
+    station_id = splits[19]
+	action = splits[15] # pickup or dropoff
+	rain = splits[1] # rain or not
+	temperature = splits[5]
+	hour = splits[14]
+	latitude = = splits[20]
+	longitude = = splits[21]
 
 	key = station_id+","+latitude+","+longitude+","+action+","+rain+","+temperature+","+hour
 	count=1
