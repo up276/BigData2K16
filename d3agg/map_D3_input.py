c70b6bd106aa02c1050308f1d6de968d987c881b
Christina <christina.bogdan@gmail.com>
2016-05-12 22:37:03 -0400

    more scripts

--- d3agg/map_D3_input.py
+++ d3agg/map_D3_input.py
@@ -1,21 +1,30 @@
 #!/usr/bin/python
+import re
 import sys
+
+def d_sub(x):
+    dec_regex = '\..*'
+    x = re.sub(dec_regex,"",str(x))
+    return x
 
 for row in sys.stdin:
     row = row.strip()
     splits=row.split(",")
 
+
  	# Change according to correct data location
-    station_id = splits[20]
+    if splits[20]=='station_id':
+	continue
+    station_id = str(int(splits[20]))
     action = splits[16] # pickup or dropoff
-    rain = splits[1] # rain or not
-    temperature = splits[3]
-    hour = splits[15]
+    rain = d_sub(splits[1]) # rain or not
+    temperature = d_sub(splits[3])
+    hour = d_sub(splits[15])
     latitude =  splits[21]
     longitude =  splits[22]
     station_name =  splits[23]
-    gender =  splits[26]
+    gender =  d_sub(splits[26])
 
-    key = station_id+","+station_name+","+latitude+","+longitude+","+action+","+rain+","+temperature+","+hour
+    key = station_id+","+station_name+","+latitude+","+longitude+","+action+","+rain+","+temperature+","+hour+","+gender
     count=1	
     print("%s\t%d"%(key, count))
--- d3agg/map_months.py
+++ d3agg/map_months.py
@@ -1,15 +1,25 @@
 #!/usr/bin/python
+import re
 import sys
 from datetime import datetime
+
+def d_sub(x):
+    dec_regex = '\..*'
+    x = re.sub(dec_regex,"",str(x))
+    return x
+
+
 
 for row in sys.stdin:
     row = row.strip()
     splits=row.split(",")
 
  	# Change according to correct data location
-    station_id = splits[20]
+    station_id = d_sub(splits[20])
+    if station_id=='station_id':
+	continue
     action = splits[16] # pickup or dropoff
-    rain = splits[1] # rain or not
+    rain = d_sub(splits[1]) # rain or not
     temperature = splits[3]
     hour = splits[15]
     latitude =  splits[21]
@@ -25,8 +35,8 @@ for row in sys.stdin:
 	wkday = 'wkday'
     else:
 	wkday =  str(datetime(int(year),int(month),int(day)).weekday())
-    gender =  splits[26]
+    gender =  d_sub(splits[26])
 
-    key = wkday+","+station_id+","+station_name+","+latitude+","+longitude+","+action+","+rain+","+temperature+","+","+gender
+    key = hour+","+station_id+","+station_name+","+latitude+","+longitude+","+action+","+rain+","+temperature
     count=1	
     print("%s\t%d"%(key, count))
